4b571397770b351a10018387fbbbb9d8289f1de3
idbditc@gmail.com <idbditc@gmail.com>
2025-11-09 18:41:17 +0600

    add identity with file upload

---
+++ AuthWebApi/Controllers/ManageController.cs
@@ -0,0 +1,101 @@
+﻿using AuthWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AuthWebApi.Controllers
+{
+  [Route("[controller]/[action]")]
+  [ApiController]
+  public class ManageController(IConfiguration config, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager) : ControllerBase
+  {
+    [ActionName("Users"), HttpGet]
+
+    public async Task<IActionResult> GetUsers()
+    {
+      var data = await userManager.Users.ToListAsync();
+      return Ok( data.Select(u=> new { u.Id, u.UserName, u.Email, u.PhotoPath, u.PhoneNumber}));
+    }
+
+   
+    [ActionName("Roles"), HttpGet]
+
+    public async Task<IActionResult> GetRoles()
+    {
+      var data = await roleManager.Roles.ToListAsync();
+
+      return Ok(data.Select(r=> new { r.Id, r.Name }));
+    }
+
+
+    [ActionName("Roles"), HttpGet("{userName}")]
+
+    public async Task<IActionResult> GetRolesByUser(string userName)
+    {
+      var user = await userManager.FindByNameAsync(userName);
+
+      if (user is null)
+      {
+        return BadRequest("invalid user");
+      }
+      var roles = await userManager.GetRolesAsync(user);
+
+      return Ok(roles);
+    }
+
+    [ActionName("Users"), HttpGet("{roleName}")]
+
+    public async Task<IActionResult> GetUsersByRole(string roleName)
+    {
+      var users = await userManager.GetUsersInRoleAsync(roleName);
+
+      return Ok(users.Select(u => new { u.Id, u.UserName, u.Email, u.PhotoPath, u.PhoneNumber }));
+    }
+
+
+
+    [ActionName("SaveRole"), HttpPost]
+
+    public async Task<IActionResult> CreateRoles(string roleName)
+    {
+      var result = await roleManager.CreateAsync(new IdentityRole(roleName));
+      if (result.Succeeded)
+      {
+        return Ok($"{roleName} role created successfully...");
+      }
+      return BadRequest(result.Errors);
+    }
+
+
+    [HttpPost]
+
+    public async Task<IActionResult> AssignRole(UserRoleAssignDto data)
+    {
+      try
+      {
+
+        var user = await userManager.FindByNameAsync(data.UserName);
+
+        if(user is null)
+        {
+          return BadRequest("invalid user");
+        }
+
+        var oldRoles = await userManager.GetRolesAsync(user);
+        await userManager.RemoveFromRolesAsync(user, oldRoles);
+        await userManager.AddToRolesAsync(user, data.Roles);
+
+
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err);
+      }
+      return Ok(data);
+    }
+
+
+  }
+}
---
+++ AuthWebApi/Services/ImageUpload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthWebApi.Services
+{
+  public class ImageUpload(IWebHostEnvironment host) : IImageUpload
+  {
+
+    public async Task<string?> UploadFile(IFormFile file, CancellationToken C)
+    {
+      if (file == null) return null;
+
+      string fileName = $@"\images\{file.FileName}";
+
+      string uploadPath = $"{host.WebRootPath}{fileName}";
+
+      using var stream = File.Create(uploadPath);
+
+      await file.CopyToAsync(stream, C);
+
+
+
+
+
+      return fileName;
+
+    }
+
+  }
+}
